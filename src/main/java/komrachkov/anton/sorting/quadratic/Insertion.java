146937004d7afbe42c71a845b9de69ae89eec182
Anton Komrachkov <a.komrachkov@gmail.com>
2022-12-09 02:14:45 +0600

    add Quadratic Insertion Sorting

---
+++ src/main/java/komrachkov/anton/sorting/quadratic/Insertion.java
@@ -0,0 +1,38 @@
+package komrachkov.anton.sorting.quadratic;
+
+import java.util.Arrays;
+
+/**
+ * @author Anton Komrachkov
+ * @since (09.12.2022)
+ */
+
+public class Inserts {
+    static int[] numbers = {5, 6, 2, 3, 9, -1};
+
+    public static void main(String[] args) {
+        sortingByInserts();
+    }
+
+    public static void sortingByInserts() {
+        System.out.println("Имеем: " + Arrays.toString(numbers));
+        int step = 0;
+        for (int i = 1; i < numbers.length; i++) {
+            for (int j = i; j > 0; j--) {
+                if (numbers[j] < numbers[j - 1]) {
+                    System.out.println("\t↓  Меняем местами числа под номерами: " + (j - 1) + " и " + j
+                            + " (" + numbers[j - 1] + " <-> " + numbers[j] + ")");
+                    int setValue = numbers[j - 1];
+                    numbers[j - 1] = numbers[j];
+                    numbers[j] = setValue;
+                    step++;
+                    System.out.println("Шаг " + step + ": " + Arrays.toString(numbers));
+                } else {
+                    System.out.println("\t|  Числа под номерами: " + (j - 1) + " и " + j
+                            + " (" + numbers[j - 1] + " < " + numbers[j] + ") на своих местах");
+                }
+
+            }
+        }
+    }
+}
--- src/main/java/komrachkov/anton/sorting/quadratic/Selection.java
+++ src/main/java/komrachkov/anton/sorting/quadratic/Selection.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 /**
  * <b>Сортировка выбором</b><br>
  * Идея: мы можем n раз выбрать минимум и составить отсортированный массив
+ *
  * @author Anton Komrachkov
  * @since (09.12.2022)
  */
@@ -13,16 +14,14 @@ public class Choice {
     static int[] numbers = {5, 6, 2, 3, 9, -1};
 
     public static void main(String[] args) {
-        sortingByChoice(numbers);
+        System.out.println("Сортировка выбором:");
+        sortingByChoice();
     }
 
-    public static void sortingByChoice(int[] numbers) {
-        System.out.println(Arrays.toString(numbers));
+    public static void sortingByChoice() {
+        System.out.println("Имеем: " + Arrays.toString(numbers));
 
-        for (int i = 0; i < numbers.length; i++) {
-
-            System.out.println("\t↓");
-
+        for (int i = 0; i < numbers.length - 1; i++) {
             int minValueIndex = -1;
             for (int j = i; j < numbers.length; j++) {
                 if (minValueIndex == -1) {
@@ -33,11 +32,15 @@ public class Choice {
                     minValueIndex = j;
                 }
             }
-            int valueByI = numbers[i];
-            numbers[i] = numbers[minValueIndex];
-            numbers[minValueIndex] = valueByI;
+            int setValue = numbers[i];
 
-            System.out.println(Arrays.toString(numbers));
+            System.out.println("\t↓  Меняем местами числа под номерами: " + i + " и " + minValueIndex
+                    + " (" + numbers[i] + " <-> " + numbers[minValueIndex] + ")");
+
+            numbers[i] = numbers[minValueIndex];
+            numbers[minValueIndex] = setValue;
+
+            System.out.println("Шаг " + (i + 1) + ": " + Arrays.toString(numbers));
         }
     }
 }
