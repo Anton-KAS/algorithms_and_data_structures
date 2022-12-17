3685f30d80ccc7f45a999e53267659ffbc385f9d
Anton Komrachkov <a.komrachkov@gmail.com>
2022-12-17 18:38:07 +0600

    T02 done

--- src/main/java/kas/anton/tasks/internship_spring_2022/T02.java
+++ src/main/java/kas/anton/tasks/internship_spring_2022/T02.java
@@ -8,13 +8,28 @@ import java.util.Scanner;
  */
 
 /*
+Стажировка весна 2022. Задача 2
 Полное условие задачи в doc/T01.png
  */
 public class T02 {
     public static void main(String[] args) {
+        long n, m; // // 1 ≤ n, m ≤ 10^18
         try (Scanner scanner = new Scanner(System.in)) {
+            n = scanner.nextLong();
+            m = scanner.nextLong();
         }
-        int result = 0;
+        long result = 0;
+        while(true) {
+            long min = Math.min(m, n);
+            long max = Math.max(m, n);
+            if (max % min == 0) {
+                result += Math.max(m, n) / Math.min(m, n);
+                break;
+            }
+            m = max - min;
+            n = min;
+            result++;
+        }
         System.out.println(result);
     }
 }
